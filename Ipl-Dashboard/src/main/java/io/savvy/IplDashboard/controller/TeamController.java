9593d31edb6b1a6d45b3882538610c7664f42101
savvylalit <savvylalit@gmail.com>
2022-04-06 13:22:02 +0530

    Backend : Added Match Api

--- Ipl-Dashboard/src/main/java/io/savvy/IplDashboard/controller/TeamController.java
+++ Ipl-Dashboard/src/main/java/io/savvy/IplDashboard/controller/TeamController.java
@@ -1,10 +1,15 @@
 package io.savvy.IplDashboard.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.savvy.IplDashboard.model.Match;
 import io.savvy.IplDashboard.model.Team;
 import io.savvy.IplDashboard.repository.MatchRepository;
 import io.savvy.IplDashboard.repository.TeamRepository;
@@ -12,9 +17,9 @@ import io.savvy.IplDashboard.repository.TeamRepository;
 @RestController
 @CrossOrigin
 public class TeamController {
-	
+
 	private TeamRepository teamRepository;
-	
+
 	private MatchRepository matchRepository;
 
 	public TeamController(TeamRepository teamRepository, MatchRepository matchRepository) {
@@ -27,7 +32,16 @@ public class TeamController {
 		Team team = this.teamRepository.findByTeamName(teamName);
 
 		team.setMatches(matchRepository.findLatestMatchesbyTeam(teamName, 4));
-		
+
 		return team;
+	}
+
+	@GetMapping("/team/{teamName}/matches")
+	public List<Match> getMatchesForTeam(@PathVariable String teamName, @RequestParam int year) {
+		LocalDate startDate = LocalDate.of(year, 1, 1);
+		LocalDate endDate = LocalDate.of(year + 1, 1, 1);
+//		return this.matchRepository.getByTeam1AndDateBetweenOrTeam2AndDateBetweenOrderByDateDesc(teamName, startDate,
+//				endDate, teamName, startDate, endDate);
+		return this.matchRepository.getMatchesByTeamBetweenDates(teamName, startDate, endDate);
 	}
 }
