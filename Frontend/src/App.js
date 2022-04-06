671e06eceac64597aacc09043bd395fff6dc2a38
savvylalit <savvylalit@gmail.com>
2022-04-06 12:54:52 +0530

    Completed ui elements for Team Page

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { TeamPage } from "./pages/TeamPage";
+import {MatchPage} from "./pages/MatchPage"
 
 function App() {
   return (
     <div className="App">
-      <TeamPage />
+      <Router>
+        <Routes>
+          <Route path="/teams/:teamName" element={<TeamPage />} />
+          <Route path="/teams/:teamName/matches/:year" element={<MatchPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
